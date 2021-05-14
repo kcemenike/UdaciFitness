2dbcdbcf2e1f3ebdc80e9ae25431438e3e4c7c6d
KC <noreply@kcemenike.github.com>
2021-05-14 03:29:42 +0100

    add touchables

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {
-  View, StyleSheet, Text, TouchableHighlight, TouchableNativeFeedback, TouchableOpacity, TouchableWithoutFeedback
+  View, StyleSheet, Text,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  TouchableWithoutFeedback
 } from 'react-native'
 import AddEntry from './components/AddEntry'
 
@@ -11,10 +15,44 @@ export default class App extends React.Component {
   //   debugger
   //   console.log('after')
   // }
+  handlePress = () => {
+    alert('Hello')
+  }
   render() {
     return (
       <View style={styles.container}>
-        <AddEntry />
+        {/* <AddEntry /> */}
+        <TouchableHighlight
+          style={styles.btn}
+          onPress={this.handlePress}
+          underlayColor='#d427b'
+        >
+          <Text style={styles.btnText}>Touchable Highlight</Text>
+        </TouchableHighlight>
+
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={this.handlePress}
+        >
+          <Text style={styles.btnText}>Touchable Opacity</Text>
+        </TouchableOpacity>
+
+        <TouchableWithoutFeedback
+          onPress={this.handlePress}
+        >
+          <View style={styles.btn}>
+            <Text style={styles.btnText}>Touchable without Feedback</Text>
+          </View>
+        </TouchableWithoutFeedback>
+
+        <TouchableNativeFeedback
+          background={TouchableNativeFeedback.SelectableBackground()}
+          onPress={this.handlePress}
+        >
+          <View style={styles.btn}>
+            <Text style={styles.btnText}>Touchable native Feedback</Text>
+          </View>
+        </TouchableNativeFeedback>
       </View>
     )
   }
@@ -28,4 +66,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  btn: {
+    backgroundColor: '#e52334',
+    padding: 10,
+    paddingLeft: 50,
+    paddingRight: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    margin: 10,
+  },
+  btnText: {
+    color: '#fff'
+  }
 });
